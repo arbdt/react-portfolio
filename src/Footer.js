2c68788e656aff228cdc543a4f9be2aac698e787
arbdt <67365149+arbdt@users.noreply.github.com>
2021-01-11 12:19:28 +1030

    uploading Footer component

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+// imports
+import React from "react";
+
+// component definition
+function Footer(){
+    return (
+        <footer className="footer">
+            <p>&copy; 2020</p>
+        </footer>
+    )
+}
+
+// export component
+export default Footer;
