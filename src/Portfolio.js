ac300f555e6d3b9153c0fd7daebbb564d30f1a62
arbdt <67365149+arbdt@users.noreply.github.com>
2021-01-11 17:19:13 +1030

    created Portfolio page component

---
+++ src/Portfolio.js
@@ -0,0 +1,12 @@
+// imports
+import React from "react";
+
+// define component
+function Portfolio(){
+    return (
+        <div><p>Portfolio under construction.</p></div>
+    );
+}
+
+// export component
+export default Portfolio;
