3cd824ab92351cdbcf5de201c9618a198edac1d4
arbdt <67365149+arbdt@users.noreply.github.com>
2021-01-12 15:47:19 +1030

    adjusted header links

--- src/Header.js
+++ src/Header.js
@@ -5,13 +5,13 @@ import {Link, useLocation} from "react-router-dom";
 // component definition
 function Header(){
 
-    const location = useLocation(); // get location from router
+    let location = useLocation(); // get location from router
 
     return (
         <header>
         {/* navbar */}
         <nav className="navbar navbar-expand-lg navbar-dark bg-custom">
-            <Link className="navbar-brand" to="/index">Daniel Arbon's Portfolio</Link> {/* navbar heading */}
+            <Link className="navbar-brand" to="/">Daniel Arbon's Portfolio</Link> {/* navbar heading */}
             
             {/* collapsed navbar button */}
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -21,13 +21,13 @@ function Header(){
             <div className="collapse navbar-collapse" id="navbarSupportedContent"> {/* expanded navbar content */}
                 <ul className="navbar-nav ml-auto">
                     <li className="nav-item">
-                        <Link className={location.pathname === "/index" ? "nav-link-active" : "nav-link" } to="/index">About Me</Link>
+                        <Link className={location.pathname === "/" ? "nav-link active" : "nav-link" } to="/">About Me</Link>
                     </li>
                     <li className="nav-item">
-                        <Link className={location.pathname === "/portfolio" ? "nav-link-active" : "nav-link" } to="/portfolio">My Portfolio</Link>
+                        <Link className={location.pathname === "/portfolio" ? "nav-link active" : "nav-link" } to="/portfolio">My Portfolio</Link>
                     </li>
                     <li className="nav-item">
-                        <Link className={location.pathname === "/contact" ? "nav-link-active" : "nav-link" } to="/contact">Contact Me</Link>
+                        <Link className={location.pathname === "/contact" ? "nav-link active" : "nav-link" } to="/contact">Contact Me</Link>
                     </li>
                 </ul>
             </div>
