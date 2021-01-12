b626723dac29721650b03f052bb38127f89ca0ca
arbdt <67365149+arbdt@users.noreply.github.com>
2021-01-13 10:24:20 +1030

    added code to portfolio page to display non-major-project items

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -5,25 +5,25 @@ import {showcaseItems, otherItems} from "./utils/PortfolioItems";
 // define component
 function Portfolio(){
     return (
-        <div class="row">
-                <div class="col-sm-12">
-                    <div class= "card mt-5"> {/* content card */}
-                        <div class="card-body mr-md-7">
+        <div className="row">
+                <div className="col-sm-12">
+                    <div className= "card mt-5"> {/* content card */}
+                        <div className="card-body mr-md-7">
                             <h1>Portfolio</h1>
                             <hr/>
                             <h3>Major Projects</h3> {/* showcase items in this section */}
                             {showcaseItems !== undefined? showcaseItems.map((item) => 
-                                <div class="row">
-                                    <div class="col-lg-12">
-                                        <div class="project-card card mb-4"> 
-                                            <img class="card-img-top" src={item.image} alt={"Preview of" + item.title}/>
-                                            <div class="card-body">
-                                                <h4 class="card-title">{item.title}</h4>
-                                                <p class="card-text">{item.text}</p>
-                                                <p class="card-text portfolio-tech">{item.tech}</p>
-                                                {item.deployedUrl !== undefined? <a href={item.deployedUrl} class="btn btn-cust-blue">Visit Site</a>: <></>}
+                                <div className="row">
+                                    <div className="col-lg-12">
+                                        <div className="project-card card mb-4"> 
+                                            <img className="card-img-top" src={item.image} alt={"Preview of " + item.title}/>
+                                            <div className="card-body">
+                                                <h4 className="card-title">{item.title}</h4>
+                                                <p className="card-text">{item.text}</p>
+                                                <p className="card-text portfolio-tech">{item.tech}</p>
+                                                {item.deployedUrl !== undefined? <a href={item.deployedUrl} className="btn btn-cust-blue">Visit Site</a>: <></>}
                                                 &emsp;
-                                                {item.repoUrl !== undefined? <a href={item.repoUrl} class="btn btn-cust-gold">Visit GitHub</a>: <></>}
+                                                {item.repoUrl !== undefined? <a href={item.repoUrl} className="btn btn-cust-gold">Visit GitHub</a>: <></>}
                                             </div>
                                         </div>
                                     </div>
@@ -31,11 +31,28 @@ function Portfolio(){
                             ): <></>}
 
                             <h3>Other Work</h3>
- 
+                            <div className="row row-cols-3">
+                                {otherItems !== undefined? otherItems.map((item) => 
+                                    <div className="col">
+                                        <div className="project-card card mb-4"> 
+                                            <img className="card-img-top" src={item.image} alt={"Preview of " + item.title}/>
+                                            <div className="card-body">
+                                                <h4 className="card-title">{item.title}</h4>
+                                                <p className="card-text">{item.text}</p>
+                                                <p className="card-text portfolio-tech">{item.tech}</p>
+                                                {item.deployedUrl !== undefined? <a href={item.deployedUrl} className="btn btn-cust-blue">Visit Site</a>: <></>}
+                                                &emsp;
+                                                {item.repoUrl !== undefined? <a href={item.repoUrl} className="btn btn-cust-gold">Visit GitHub</a>: <></>}
+                                            </div>
+                                        </div>
+                                    </div>
+                                ): <></>}
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+
     );
 }
 
