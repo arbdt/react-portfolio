53911336d600987268e8f080a51a3b3bc60a7dec
arbdt <danieltarbon@live.com.au>
2021-01-13 15:33:34 +1030

    added path basename to router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import {BrowserRouter as Router, Route} from "react-router-dom";
-import Header from "./Header";
-import About from './About';
-import Portfolio from './Portfolio';
-import Contact from './Contact';
-import Footer from "./Footer";
+import Header from "./components/Header";
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Footer from "./components/Footer";
 
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div id="appContainer">
         <Header/>
           <main className="container">
             <Route path="/portfolio" component={Portfolio}/>
             <Route path="/contact" component={Contact}/>
-            <Route path="/" component={About}/>
+            <Route exact path="/" component={About}/>
           </main>
         <Footer/>
       </div>
